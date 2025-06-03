5b900dad36698086618ad0de1700035569b19d2d
rohitmalviya03 <rmrohit439@gmail.com>
2025-06-03 20:35:18 +0530

    hi

---
+++ src/main/java/com/app/LIMS/Repository/BillRepository.java
@@ -0,0 +1,11 @@
+package com.app.LIMS.Repository;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.app.LIMS.entity.Bill;
+import com.app.LIMS.entity.Patient;
+
+public interface BillRepository extends JpaRepository<Bill, Long> {
+    List<Bill> findByPatient(Patient patient);
+}
